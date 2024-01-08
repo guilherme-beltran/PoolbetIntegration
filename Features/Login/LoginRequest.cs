6d6cf95c1c475ddca1751ef3ce0cba1fd74abf10
Guilherme Beltran <gtmbeltran@gmail.com>
2024-01-08 16:10:35 -0300

    updated LoginRequest.cs

--- Features/Login/LoginRequest.cs
+++ Features/Login/LoginRequest.cs
@@ -1,10 +1,11 @@
-﻿using Newtonsoft.Json;
+using Newtonsoft.Json;
 
 namespace PoolbetIntegration.API.Features.Login;
 
 public sealed class LoginRequest
 {
-    public string Login { get; set; } = "clubguilherme";
-    public string Password { get; set; } = "testeintegracao";
-    public int PartnerId { get; set; } = 2011;
+    public string Login { get; set; }
+    public string Password { get; set; }
+    public int PartnerId { get; set; }
+    public string Currency { get; set; }
 }
